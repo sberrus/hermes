022933737e3ba5f2eda1bffadc6ad4979ec27ad0
u029291 <samuelalberto.berrus.ext@rsi.cajarural.com>
2025-05-14 17:43:13 +0200

    Añade conexión a BBDD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { App } from '@/models/app';
+import { App } from "@/models/app";
+import { DatabaseConnector } from "./models/database";
 
-const app = new App()
-app.start()
+
+const db = new DatabaseConnector();
+db.init();
+
+// const app = new App();
+// app.start();
 
 // todo
 // [ ] implement anti brute force strategies ** express-brute library could help.
