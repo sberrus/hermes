167b1ff673b68decfc080c967ddaacc43c09763c
u029291 <samuelalberto.berrus.ext@rsi.cajarural.com>
2025-05-14 15:38:07 +0200

    Añade telemetria

--- src/controllers/mailer.controller.ts
+++ src/controllers/mailer.controller.ts
@@ -1,10 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
 // GET
 /**
  * Response with a simple ping
  */
 export const sendSimpleEmail = (req: Request, res: Response) => {
-    res.json({ msg: "email sent!" })
-}
-
+  res.json({ msg: "email sent!" });
+};
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,18 +1,48 @@
 /*instrumentation.ts*/
-import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import * as opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import dotenv from "dotenv";
+dotenv.config();
+const dynatraceTelemetryEndpoint = process.env.DYNATRACE_TELEMETRY_ENDPOINT;
+const dynatraceToken = process.env.DYNATRACE_TOKEN;
 
-const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: `${dynatraceTelemetryEndpoint}/v1/traces`,
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {
+      Authorization: `Api-Token ${dynatraceToken}`,
+    },
   }),
-  instrumentations: [getNodeAutoInstrumentations()],
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: `${dynatraceTelemetryEndpoint}/v1/metrics`, // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {
+        Authorization: `Api-Token ${dynatraceToken}`,
+      }, // an optional object containing custom headers to be sent with each request
+    }),
+  }),
+  instrumentations: [
+    // Esta configuración omite los logs que devuelve de error de dyna UNSUPPORTED_METRIC_TYPE_MONOTONIC_CUMULATIVE_SUM
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-http": {
+        enabled: true,
+        requestHook: (span) => {},
+      },
+      "@opentelemetry/instrumentation-express": {
+        enabled: true,
+      },
+    }),
+  ],
 });
 
-sdk.start();
+try {
+  sdk.start();
+  console.log("Telemetría conectada satisfactoriamente");
+} catch (error) {
+  console.log("Error:", error);
+}
